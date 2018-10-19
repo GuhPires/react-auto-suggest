2cadb23781a773ea10c4198f8abda711698e1746
Gustavo <gustavo.goncalvespires@gmail.com>
2018-10-19 13:51:00 -0300

    Clear function added

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@
     }
 
     handleClick(e){
+        console.log('Node: ', this.node);
         if(this.node && !this.node.contains(e.target)){
             this.closeSuggestions()
         }
@@ -52,24 +53,38 @@
         });
     }
 
+    clearInput(e){
+        e.stopPropagation();
+        this.setState({
+            selected: '',
+            showSuggestion: false
+        });
+    }
+
     componentWillUnmount(){
         document.removeEventListener('mousedown', this.handleClick.bind(this), false);
     }
 
     render(){
         return(
-            <div ref={node => this.node = node} className="auto-suggest-wrapper">
+            <div ref={node => this.node = node} className="auto-suggest-wrapper" style={{width: '90%'}}>
                 <input className="auto-suggest" type="text" value={this.state.selected} onChange={this.handleInputChange.bind(this)} style={{width: "100%"}} placeholder={this.props.placeholder} />
-                <div className="suggestion-wrapper" style={this.state.showSuggestion ? {display: 'flex'} : {display: 'none'}}>
+                {this.props.isClearable && this.state.selected && <span className="clear-suggestion-input" onClick={this.clearInput.bind(this)}>X</span>}
+                <div className="suggestion-wrapper" style={{width: '100%', display: this.state.showSuggestion ? 'flex' : 'none'}}>
                     {this.state.data.length > 0 && this.state.data.map((d, i) => (
                         <span key={i} className="suggestion-content" onClick={this.selectOption.bind(this, d)}>{d[this.props.inputKey]}</span>
                     ))}
-                    {this.state.data.length < 1 && <span style={{padding: '10px'}}>{this.props.noResultsText}</span>
-                    }
+                    {this.state.data.length < 1 && <span style={{padding: '10px'}}>{this.props.noResultsText}</span>}
                 </div> 
             </div>
         )
     }
+}
+
+AutoSuggest.defaultProps = {
+    isClearable: true,
+    placeholder: 'Type Something...',
+    noResultsText: 'No Results Found :('
 }
 
 ReactDOM.render(<AutoSuggest data={[{label: 'Apple', value: 'apple'}, {label: 'Banana', value: 'banana'}, {label: 'Peach', value: 'pearch'}, {label: 'Grape', value: 'grape'}, {label: 'Watermellon', value: 'watermellon'}]} inputKey="label" placeholder="Search for a fruit..." noResultsText="No results Found :("/>, document.getElementById('root'))
